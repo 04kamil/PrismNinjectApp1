ae83c99b2ae22d2c2bfe694090360c5b2b3658bc
Kamil Brożek <kamil.brozek@hotmail.com>
2017-10-20 09:05:50 +0200

    Add project files.

---
+++ PrismNinjectApp1/Bootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using Ninject;
+using Prism.Ninject;
+using PrismNinjectApp1.Views;
+using System.Windows;
+
+namespace PrismNinjectApp1
+{
+    class Bootstrapper : NinjectBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Kernel.TryGet<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
